776dbe314b1b04c714eae8362d074c6367755f09
Ryan Schader <schader.ryan41@gmail.com>
2023-01-05 17:53:45 -0700

    finihsed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What grade did you get in your class?(write a number, such as 87 or 94)");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+    if (number >= 90)
+        {
+            Console.WriteLine("you recieved an A");
+        }
+    else if (number >= 80)
+        {
+            Console.WriteLine("You recieved a B");
+        }
+    else if (number >= 70)
+        {
+            Console.WriteLine("You recieved a C");
+        }
+    else if (number >= 60)
+        {
+            Console.WriteLine("You recieved a D");
+        }
+    else
+        {
+            Console.WriteLine("You recieved an F");
+        }
+    if (number >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+    else
+        {
+            Console.WriteLine("YOU ARE A FAILURE");
+        }
     }
 }
