71a84004e252302ef2c7e663966fd3f65c2fa749
Ryan Schader <schader.ryan41@gmail.com>
2023-01-10 18:07:33 -0700

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class MainClass {
+  public static void Main(string[] args) {
+    Random rnd = new Random();
+    int randomNumber = rnd.Next(1, 101);
+    int userGuess;
+    Console.WriteLine("Please try to guess the random number between 1 and 100: ");
+    while (true) {
+    userGuess = Convert.ToInt32(Console.ReadLine());
+        if (userGuess < randomNumber) {
+            Console.WriteLine("Too Low! Guess again");
+        }
+        else if (userGuess > randomNumber) {
+            Console.WriteLine("Too High! Guess again");
+        }
+        else {
+            Console.WriteLine("Congratulations! You've guessed the number correctly.");
+            break;
+        }
     }
+  }
 }
