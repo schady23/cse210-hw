58974461607de009f67d0ca384a45643558e97de
Ryan Schader <schader.ryan41@gmail.com>
2023-01-10 18:25:43 -0700

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+class MainClass {
+    public static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    public static string PromptUserName() {
+        Console.WriteLine("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    public static int PromptUserNumber() {
+        Console.WriteLine("Please enter your favorite number: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    public static int SquareNumber(int number) {
+        return number * number;
+    }
+    public static void DisplayResult(string name, int number) {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Square of the favorite number: " + number);
+    }
+
+    public static void Main(string[] args) {
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(name, squaredNumber);
     }
 }
+
