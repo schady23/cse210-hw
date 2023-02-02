3e901615c1973f815145093f6f61d33fb60a9878
Ryan Schader <schader.ryan41@gmail.com>
2023-02-02 12:19:40 -0700

    finished learning 03

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -4,19 +4,16 @@ public class Fraction
 {
     private int numerator;
     private int denominator;
-
     public Fraction()
     {
         numerator = 1;
         denominator = 1;
     }
-
     public Fraction(int top)
     {
         numerator = top;
         denominator = 1;
     }
-
     public Fraction(int _numerator, int _denominator)
     {
         numerator = _numerator;
@@ -28,7 +25,6 @@ public class Fraction
         string text = $"{numerator}/{denominator}";
         return text;
     }
-
     public double GetDecimalValue()
     {
         return (double)numerator / (double)denominator;
