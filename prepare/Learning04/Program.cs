1873740f5f134b38dfc970c0f6552f456699a31d
Ryan Schader <schader.ryan41@gmail.com>
2023-02-15 09:49:02 -0700

    finished learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment as1 = new Assignment("Ryan Schader", "Diff Eq");
+        Console.WriteLine(as1.GetSummary());
+
+        MathAssignment as2 = new MathAssignment("Nathan Schader", "Trigonometry", "5.1", "5-10");
+        Console.WriteLine(as2.GetSummary());
+        Console.WriteLine(as2.GetHomeworkList());
+
+        WritingAssignment as3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(as3.GetSummary());
+        Console.WriteLine(as3.GetWritingInformation());
     }
 }
